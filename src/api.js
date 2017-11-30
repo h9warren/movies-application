416c1b18d937b5a13c628ced557c22a2349509e6
Hunter Warren <H9@The-Icon.local>
2017-11-29 21:16:52 -0600

    project files

--- src/api.js
+++ src/api.js
@@ -2,5 +2,14 @@ module.exports = {
   getMovies: () => {
     return fetch('/api/movies')
       .then(response => response.json());
+  },
+  addMovie: () => {
+  	console.log("add");
+  },
+  editMovie: () => {
+  	console.log("edit");
+  },
+  deleteMovie: () => {
+  	console.log("delete");
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 /**
  * es6 modules and imports
  */
+const $ = require('jquery');
 import sayHello from './hello';
 sayHello('World');
 
 /**
  * require style imports
  */
-const {getMovies} = require('./api.js');
+const { getMovies, addMovie, editMovie, deleteMovie } = require('./api.js');
+// const results = document.getElementById("results");
+addMovie();
+editMovie();
+deleteMovie();
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+    $("#results").append(`<h2>id#${id} - ${title} - rating: ${rating}</h2>`);
+
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
